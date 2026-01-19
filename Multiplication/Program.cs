4d18f5061f366f9da31b20b77494f27ddb48e28a
Shreya Singh <shreya08079@gmail.com>
2026-01-19 12:19:23 +0530

    Added

---
+++ Multiplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter upto: ");
+        int range = int.Parse(Console.ReadLine());
+
+        int[] result = MultiplicationTable(n, range);
+
+        Console.WriteLine(string.Join(", ", result));
+    }
+
+    static int[] MultiplicationTable(int n, int range)
+    {
+        int[] row = new int[range];
+
+        for (int i = 1; i <= range; i++)
+        {
+            row[i - 1] = n * i;
+        }
+
+        return row;
+    }
+}
